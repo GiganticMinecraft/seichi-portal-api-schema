dbd4e30be49163fefb4f5c1e40210ff377bf6326
rito528 <39003544+rito528@users.noreply.github.com>
2024-06-14 14:44:27 +0900

    chore: API定義をtypespecに置き換える

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-redocly build-docs /schema/openapi.yml -o ./docs/index.html -t ./template.hbs
+tsp compile .
+
+redocly build-docs ./tsp-output/@typespec/openapi3/openapi.yaml -o ./docs/index.html -t ./template.hbs
 http-server ./docs -p $PORT
