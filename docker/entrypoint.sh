048502cbee522e2ea5a8f55e5766ee4d05bddd14
rito528 <39003544+rito528@users.noreply.github.com>
2024-07-06 17:53:48 +0900

    chore: TypeSpecコードをsrcディレクトリの中にまとめる

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tsp compile .
+tsp compile ./src
 
 redocly build-docs ./tsp-output/@typespec/openapi3/openapi.yaml -o ./docs/index.html -t ./template.hbs
 http-server ./docs -p $PORT
