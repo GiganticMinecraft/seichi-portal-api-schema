4cf4967e0a506981c48841d74be9c684485f2fc1
Lucky <yubi@clov3r.cc>
2024-09-08 11:02:25 +0900

    fix: entrypoint.shの中でtspのビルドは不要なので修正

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-tsp compile ./src
-
-redocly build-docs ./tsp-output/@typespec/openapi3/openapi.yaml -o ./docs/index.html -t ./template.hbs
+redocly build-docs /schema/openapi.yaml -o ./docs/index.html -t ./template.hbs
 http-server ./docs -p $PORT
